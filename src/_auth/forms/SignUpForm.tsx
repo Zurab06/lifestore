cf79b507c1451665d1c9e241d9f047c3a4e9b154
John Doe <johndoe@example.com>
2024-02-01 11:14:40 +0300

    creating a project"

---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/components/ui/button"
+const SignUpForm = () => {
+  return (
+    <div >
+      <Button>click me</Button>
+    </div>
+  )
+}
+
+export default SignUpForm

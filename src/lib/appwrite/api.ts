5546851220f1108d2d8f5d70517aef0566cb03e9
John Doe <johndoe@example.com>
2024-04-29 22:04:37 +0300

    added signing

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -71,3 +71,14 @@ export async function getCurrentUser() {
     
   }
 }
+
+export async function signOutAccount(){
+  try {
+    const session = await account.deleteSession('current')
+    return session
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+  
--- src/lib/react-query/queriesAndMutatuions.ts
+++ src/lib/react-query/queriesAndMutatuions.ts
@@ -1,18 +1,28 @@
-import { INewUser } from "@/types"
-import { useQuery,useMutation,useInfiniteQuery,useQueryClient } from "@tanstack/react-query"
-import { createUserAccount,signInAccount } from "../appwrite/api"
+import { INewUser } from "@/types";
+import {
+  useQuery,
+  useMutation,
+  useInfiniteQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
 
+export const useCreateAccount = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+export const useSignInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
 
-export const useCreateAccount = ()=>{
-    return useMutation({
-        mutationFn:(user:INewUser)=> createUserAccount(user)
-    })
-}
-export const useSignInAccount = ()=>{
-    return useMutation({
-        mutationFn:(user:{
-            email: string,
-            password: string
-        })=> signInAccount(user)
-    })
-}
+export const useSignOutAccount = () => {
+  return useMutation({ mutationFn: signOutAccount });
+};
